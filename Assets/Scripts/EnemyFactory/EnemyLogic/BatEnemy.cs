567e4785c13af2eea36a41b4c35642588a66b8e3
Sidharth <sidharthsuresh07@gmail.com>
2025-10-22 09:27:07 -0400

    moved base and made enemy logic

---
+++ Assets/Scripts/EnemyFactory/EnemyLogic/BatEnemy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BatEnemy : EnemyBase
+{
+    private protected override void OnSpawn()
+    {
+        throw new System.NotImplementedException(); // add logic here
+    }
+}
---
+++ Assets/Scripts/EnemyFactory/EnemyLogic/WalkerEnemy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class WalkerEnemy : EnemyBase
+{
+    private protected override void OnSpawn()
+    {
+        throw new System.NotImplementedException(); // add logic here
+    }
+}
