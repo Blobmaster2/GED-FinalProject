909f838a5e2612fb22b2877b04a6942485288c8d
Sidharth <sidharthsuresh07@gmail.com>
2025-10-22 09:15:56 -0400

    added info about wave spawning layed out some comments so other devs can read better through my code and undertsand what is going on

--- Assets/Scripts/EnemyFactory/WaveSpawner.cs
+++ Assets/Scripts/EnemyFactory/WaveSpawner.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
+    [SerializeField] private int spawnCount; // how many enemies to spawn according to the wave
+    [SerializeField] private float waveBasedSpawnScale; // scaling between spawn count on each wave
+                                                        // (eg. first wave -> 10 enemies
+                                                        // while second wave -> 12 enemies;
+                                                        // thereby giving it a 1.2 increase in between spawns.)
+    [SerializeField] private int spawnLevel; // level of enemies that are spawning
+    [SerializeField] private float timeBetweenSpawns; // time between spawns (not time between waves as time between
+                                                      // waves will remain the same every wave
+    [SerializeField] private int spawnRadius; // the spawn radius around the player that the enemies are allowed to
+                                              // spawn in
+    
+    private Vector2 destinationPosition = new Vector2(); // this variable is to replaced with a *global variable* that
+                                                         // keeps track of the player position
+    
+    // this script should run a timer and spawn enemies according to the level and abilities of the player
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +29,10 @@ public class WaveSpawner : MonoBehaviour
     {
         
     }
+    
+    private void SpawnNextWave()
+    {
+        
+    }
+
 }
