a46d9c1e2b5d2f2b0e7b114744f25135aed37c0a
Sidharth <sidharthsuresh07@gmail.com>
2025-10-22 08:00:24 -0400

    Make EnemyBase abstract and add core properties Changed EnemyBase to an abstract class, added serialized fields for health, speed, and attack properties, and implemented movement logic towards a destination. Introduced OnSpawn as an abstract method for derived classes to handle spawn behavior.

--- Assets/Scripts/EnemyFactory/EnemyBase.cs
+++ Assets/Scripts/EnemyFactory/EnemyBase.cs
@@ -1,16 +1,28 @@
+using UnityEditor.UI;
 using UnityEngine;
 
-public class EnemyBase : MonoBehaviour
+public abstract class EnemyBase : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float health;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float speed;
+    [SerializeField] private float attackCoolDown;
+    [SerializeField] private float attackDamage;
+
+    private Vector2 destinationPosition = new Vector2(); // this variable is to replaced with a *global variable* that
+                                                         // keeps track of the player position
+    
+    private void Start()
     {
-        
+        health = maxHealth;
+        OnSpawn();
     }
 
-    // Update is called once per frame
-    void Update()
+    private protected abstract void OnSpawn();
+
+    private void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, destinationPosition, 
+            speed * Time.deltaTime);
     }
 }
