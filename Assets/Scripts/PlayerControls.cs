87b8d908cb435ff0910301eaaf71bc82e2bdaddd
Blobmaster2 <61041829+Blobmaster2@users.noreply.github.com>
2025-10-22 00:24:16 -0400

    bug fix for dll (editor class not showing up)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
-using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
