43cc7030f218e6c58072b28d7c124edb843ca928
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-11-26 01:57:21 -0500

    dopooling variable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool DoPooling = true;
     private static GameManager Instance { get; set; }
 
     private int playerLevel;
